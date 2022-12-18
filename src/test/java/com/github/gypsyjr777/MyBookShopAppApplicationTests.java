256238f10695abe90062b508b3620f42d7ea802c
GypsyJR777 <ivan.demidov.01@list.ru>
2022-12-18 17:59:53 +0300

    Simple unit test

--- src/test/java/com/github/gypsyjr777/MyBookShopAppApplicationTests.java
+++ src/test/java/com/github/gypsyjr777/MyBookShopAppApplicationTests.java
@@ -1,13 +1,34 @@
 package com.github.gypsyjr777;
 
+import org.hamcrest.Matchers;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
 
 @SpringBootTest
 class MyBookShopAppApplicationTests {
+	@Value("${auth.secret}")
+	String authSecret;
+
+	private final MyBookShopAppApplication application;
+
+	@Autowired
+	public MyBookShopAppApplicationTests(MyBookShopAppApplication application) {
+		this.application = application;
+	}
 
 	@Test
 	void contextLoads() {
+		assertNotNull(application);
+	}
+
+	@Test
+	void verifyAuthSecret() {
+		assertThat(authSecret, Matchers.containsString("box"));
 	}
 
 }
